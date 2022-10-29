5b110aaa7dcb4e5ec382c52fe491641d3e51a46e
Raghu0701 <dhyeydave@hotmail.com>
2022-10-30 01:43:42 +0530

    Created User API

---
+++ app/user/views.py
@@ -0,0 +1,13 @@
+"""
+VIews for the user API.
+"""
+
+from rest_framework import generics
+
+from user.serializers import UserSerializer
+
+
+class CreateUserView(generics.CreateAPIView):
+    """Create a new user in system."""
+
+    serializer_class = UserSerializer
